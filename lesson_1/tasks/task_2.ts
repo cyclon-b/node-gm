9c1b997d3e726ad3d03dd5900be83b8f958debbf
TOOL <artgenn@mail.ru>
2021-03-20 15:16:09 +0300

    reworked read and write with streams

--- lesson_1/tasks/task_2.ts
+++ lesson_1/tasks/task_2.ts
@@ -1,32 +1,25 @@
 import * as csv from 'csvtojson';
 import * as fs from 'fs';
 import * as path from 'path';
-import * as stream from 'stream';
+import { Readable } from 'stream';
 
-const CSV_PATH = path.join(__dirname, 'csv/hw1.csv1');
-const TXT_PATH = path.join(__dirname, 'txt/hw1.txt');
-const readable = new stream.Readable();
+const CSV_PATH = path.join(__dirname, 'csv/hw1.csv');
+const TXT_PATH = path.join(__dirname, 'txt/hw.txt');
 
+const readableStream = Readable.from(csv().fromFile(CSV_PATH).on('data', (data) => data));
+const writableStream = fs.createWriteStream(TXT_PATH);
 
-const convertCsvToJson = (pathToFile): any => {
-    return csv().fromFile(pathToFile).on('data', (data: object[]) => readable._read(64).push(data));
-};
+readableStream.on('data', (chunk) => {
+    writableStream.write(chunk.toString());
+    readableStream.emit('close');
+    writableStream.emit('close');
+});
 
-const writeJsonTxtFile =  () => {
-    readable.on('data', (data) => console.dir(data))
-
-//     fs.appendFile(TXT_PATH, JSON.stringify(jsonData), (err) => {
-//         if (err) {
-//             console.log(`Sorry we have an error(( ${err}`)
-//         }
-//         console.log(`File hw1.txt was appended`);
-//     });
-// }
-
-// convertCsvToJson(CSV_PATH)
-//     .then(json => writeJsonTxtFile(json))
-//     .catch(err => `Sorry we have an error(( ${err}`);
-}
-
-convertCsvToJson(CSV_PATH);
-writeJsonTxtFile();
+readableStream.on('error', (err: Error) => {
+    console.log(err.message);
+    readableStream.emit('close');
+});
+writableStream.on('error', (err: Error) => {
+    writableStream.emit('close');
+    console.log(err.message);
+});
