5264a95ed3109db4f8a8b6e3be9983d9604fdf27
Gennady Artamonov <artgenn@mail.ru>
2021-03-22 16:06:09 +0300

    Simplify converter

--- lesson_1/tasks/task_2.ts
+++ lesson_1/tasks/task_2.ts
@@ -1,29 +1,24 @@
 import * as csv from 'csvtojson';
 import * as fs from 'fs';
 import * as path from 'path';
-import { Readable } from 'stream';
 
 const CSV_PATH = path.join(__dirname, 'csv/hw1.csv');
 const TXT_PATH = path.join(__dirname, 'txt/hw.txt');
 
-const readableStream = Readable.from(csv().fromFile(CSV_PATH).on('data', (data) => data));
+const readableStream = fs.createReadStream(CSV_PATH);
 const writableStream = fs.createWriteStream(TXT_PATH);
 
 
 const convertCsvToTxt = (): void => {
-    readableStream.on('data', (chunk) => {
-        writableStream.write(chunk.toString());
-        readableStream.emit('close');
-        writableStream.emit('close');
-    });
+   readableStream.pipe(csv()).pipe(writableStream);
 
     readableStream.on('error', (err: Error) => {
-        console.log(err.message);
+        console.log(`READ ERROR! ${err.message}`);
         readableStream.emit('close');
     });
     writableStream.on('error', (err: Error) => {
         writableStream.emit('close');
-        console.log(err.message);
+        console.log(`WRITE ERROR! ${err.message}`);
     });
 
 }
